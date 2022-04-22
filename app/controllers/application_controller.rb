3b8e721e0b121f19a4ab3323bf5a2681f8f6aabe
tcui18 <tiffanyc.1262@gmail.com>
2022-04-22 21:14:43 +0000

    classsss work done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,56 @@
 class ApplicationController < ActionController::Base
+  def add_form
+    render({ :template => "form_templates/add_form.html.erb" })
+  end
 
+  def add_results
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    render({ :template => "math_templates/add_results.html.erb" })
+  end
+
+  def subtract_form
+    render({ :template => "form_templates/subtract_form.html.erb" })
+  end
+
+  def subtract_results
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    render({ :template => "math_templates/subtract_results.html.erb" })
+  end
+
+  def multiply_form
+    render({ :template => "form_templates/multiply_form.html.erb" })
+  end
+
+  def multiply_results
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    render({ :template => "math_templates/multiply_results.html.erb" })
+  end
+
+  def divide_form
+    render({ :template => "form_templates/divide_form.html.erb" })
+  end
+
+  def divide_results
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    render({ :template => "math_templates/divide_results.html.erb" })
+  end
+
+  def street_to_coords_form
+    render({ :template => "coords/street_to_coords.html.erb" })
+  end
+
+  def street_to_coords_results
+    @address = params.fetch("addr")
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @address + "&key=AIzaSyD8RrOFB0dPsF-leqeFJdmX3yOvcQbfNyY"
+    @data = open(url).read
+    first_result = JSON.parse(@data).fetch("results")[0]
+    @location = first_result.fetch("geometry").fetch("location")
+    @lat = @location.fetch("lat")
+    @lng = @location.fetch("lng")
+    render({ :template => "coords/coords.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get("/add", { :controller => "application", :action => "add_form" })
+  get("/wizard_add", { :controller => "application", :action => "add_results" })
+
+  get("/subtract", { :controller => "application", :action => "subtract_form" })
+  get("/wizard_subtract", { :controller => "application", :action => "subtract_results" })
+
+  get("/multiply", { :controller => "application", :action => "multiply_form" })
+  get("/wizard_multiply", { :controller => "application", :action => "multiply_results" })
+
+  get("/divide", { :controller => "application", :action => "divide_form" })
+  get("/wizard_divide", { :controller => "application", :action => "divide_results" })
+
+  get("/street_to_coords", { :controller => "application", :action => "street_to_coords_form" })
+  get("/street_to_coords/results", { :controller => "application", :action => "street_to_coords_results" })
 end
